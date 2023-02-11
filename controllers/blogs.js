6d9b2b2df2fd6a7fe5c4b22aad9cf3482197bec1
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-02-11 21:31:00 +0200

    4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,19 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
+
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  console.log(blog)
+
+  const user = await User.findById(request.body.userId)
+  blog.user = user._id
 
   if (blog.title === undefined || blog.url === undefined) {
     return response.status(400).json({ error: 'content missing' })
@@ -15,9 +21,15 @@ blogRouter.post('/', async (request, response) => {
   else {
     blog.likes = blog.likes || 0
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
   }
 })
+
+
+
 
 blogRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
