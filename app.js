7db270da4d3bdee039b35fd0071f397717c16d21
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-02-11 20:11:26 +0200

    4.15

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
-const { requestLogger, unknownEndpoint } = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
+const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 app.use(unknownEndpoint)
-
+app.use(errorHandler)
 
 
 module.exports = app
