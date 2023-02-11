606a130e2798477559685a5ebbbd00f991f53bf6
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-02-11 17:47:42 +0200

    kaikki 4.14 asti

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,10 +9,32 @@ blogRouter.get('/', async (request, response) => {
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+  if (blog.title === undefined || blog.url === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+  else {
+    blog.likes = blog.likes || 0
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
 
 
 module.exports = blogRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,84 +1,153 @@
 const mongoose = require('mongoose')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blogs')
 
-const initialBlogs = [
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-  {
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-  },
-]
-
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+  await Blog.insertMany(helper.initialBlogs)
 })
 
-test('right number of blogs in JSON format', async () => {
-  const response = await api.get('/api/blogs')
-  expect(200)
-  expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
 
-test('returned blogs have field id', async () => {
-  const response = await api.get('/api/blogs')
+describe('Test retrieving all blogs', () => {
+  //4.8
+  test('right number of blogs in JSON format', async () => {
+    const response = await api.get('/api/blogs')
+    expect(200)
+    expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-  const ids = response.body.map(blog => blog.id)
-  const filtered = ids.filter(e => e) // filter away all undefined ids
-  const fetchIds = (filtered) => {
+  //4.9
+  test('returned blogs have field id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const ids = response.body.map(blog => blog.id)
+    const filtered = ids.filter(e => e) // filter away all undefined ids
+    const fetchIds = (filtered) => {
     // there can not be less defined ids than blogs in database
-    if (filtered.length < initialBlogs.length) {
-      return undefined
-    } else {
-      return filtered
+      if (filtered.length < helper.initialBlogs.length) {
+        return undefined
+      } else {
+        return filtered
+      }
     }
-  }
 
-  expect(fetchIds(filtered)).toBeDefined()
+    expect(fetchIds(filtered)).toBeDefined()
+  })
+
+})
+
+describe('Test adding blogs', () => {
+//4.10
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const urls = blogsAtEnd.map(r => r.url)
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(urls).toContain(
+      'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll'
+    )
+  })
+
+  // 4.11*
+  test('if likes is not defined it will get value 0', async () => {
+    const newBlog = {
+      title: 'Second class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2018/05/05/TestDefinitions.htmll',
+    }
+
+    const response  = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.likes).toBe(0)
+
+  })
+
+  // 4.12* 1/2
+  test('if url is not defined give status code 400', async () => {
+    const newBlog = {
+      title: 'Third class tests',
+      author: 'Robert C. Martin',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  })
+
+  // 4.12* 2/2
+  test('if title is not defined give status code 400', async () => {
+    const newBlog = {
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2018/05/05/TestDefinitions.htmll',
+      likes: 5
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  })
 })
 
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  }
+
+// 4.13
+test('a blog can be deleted', async () => {
+  const blogAtStart = await helper.blogsInDb()
+  const blogToDelete = blogAtStart[0]
 
   await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
 
-  const response = await api.get('/api/blogs')
+  const blogsAtEnd = await helper.blogsInDb()
 
-  const urls = response.body.map(r => r.url)
-
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(urls).toContain(
-    'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll'
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
   )
+
+  const urls = blogsAtEnd.map(r => r.url)
+
+  expect(urls).not.toContain(blogToDelete.url)
 })
 
+//4.14*
+test('likes in blog can be updated', async () => {
+  const blogAtStart = await helper.blogsInDb()
+  const blogToUpdate = blogAtStart[0]
+  blogToUpdate.likes = 10
 
+  const response = await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(blogToUpdate)
+    .expect(200)
 
+  const blogsAtEnd = await helper.blogsInDb()
 
-
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length
+  )
+  expect(response.body.likes).toBe(10)
+})
 
 
 
